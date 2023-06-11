c5fa568daf0596f5837aff1381aac4d157931935
okmy123 <136219898+okmy123@users.noreply.github.com>
2023-06-11 14:14:56 +0800

    Update index.ts

--- frontend/src/stores/modules/chat/index.ts
+++ frontend/src/stores/modules/chat/index.ts
@@ -28,7 +28,7 @@ export const useChatStore = defineStore(
         label: 'Bing 官方',
       },
       {
-        baseUrl: 'https://sydney.vcanbb.chat',
+        baseUrl: 'https://bingapi.51baipiao.net',
         label: 'Cloudflare',
       },
       {
